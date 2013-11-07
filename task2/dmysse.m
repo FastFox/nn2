03d6d6744478cab16b53626d9888b056323a335f
PesirNL <pesirnl@gmail.com>
2013-11-07 14:09:31 +0100

    step 4 aangemaakt en aangepast

--- task2/dmysse.m
+++ task2/dmysse.m
@@ -19,12 +19,12 @@ function [d] = dmysse(w)
     net = y1*w(5) + y2*w(6); y = phi(net);
 
     du1 = (y - d) * phiprime(net) * y1;
-	  du2 = (y - d) * phiprime(net) * y2;
+    du2 = (y - d) * phiprime(net) * y2;
 
-	  dw1 = (y - d) * phiprime(net) * u1 * phiprime(net1) * x1;
-	  dw2 = (y - d) * phiprime(net) * u1 * phiprime(net1) * x2;
+    dw1 = (y - d) * phiprime(net) * u1 * phiprime(net1) * x1;
+    dw2 = (y - d) * phiprime(net) * u1 * phiprime(net1) * x2;
 
-	  dv1 = (y - d) * phiprime(net) * u2 * phiprime(net2) * x1;
+    dv1 = (y - d) * phiprime(net) * u2 * phiprime(net2) * x1;
     dv2 = (y - d) * phiprime(net) * u2 * phiprime(net2) * x2;
 
    	 
---
+++ task2/step4.m
@@ -0,0 +1,44 @@
+function task_1()
+    starting_point = randn(100,6)
+    
+    options = foptions;         % Standard options
+    options(1) = -1;            % Turn off printing completely
+    options(3) = 1e-8;          % Tolerance in value of function
+    options(14) = 100;          % Max. 100 iterations of algorithm
+    options(18) = 0.001;         % Learning rate    
+    
+    default_options = options;
+    
+    results = zeros(100,5,4);   %experiment, algorithm, output (1=eval, 2=gradeval,3=runtime,4=successrate)
+    
+    functionList = {@graddesc, @graddesc, @scg, @conjgrad, @quasinew};
+    for i = 1:100
+        disp(i);
+        for j = 1:5
+            options = default_options;
+            if(j==1 || j==2)
+                options(18) = 0.008;
+            end
+            if(j==2)
+                options(7) = 1;
+            end
+            tic;
+            [dump, options, dump, dump] = functionList{j}('mysse', starting_point(i,:), options, 'dmysse');
+            time = toc;
+            results(i,j,1) = options(10);
+            results(i,j,2) = options(11);
+            results(i,j,3) = time;
+            results(i,j,4) = options(8);
+        end 
+    end  
+    
+    for i = 1:5
+        disp(' ');
+        disp(['Function ' num2str(i) ':']); 
+        disp(['Average function evaluations: ' num2str(mean(results(:,i,1)))]);
+        disp(['Average gradient evaluations: ' num2str(mean(results(:,i,2)))]);
+        disp(['Average runtime: ' num2str(mean(results(:,i,3)))]);
+        disp(['Average successrate: ' num2str(sum(results(:,i,4) < 0.0001)/100) '%']);
+    end
+    
+end
